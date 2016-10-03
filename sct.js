a963e2896eb09cbb1cbcb4917fd5683b95626cec
Itay Sharon <itay.sharon100@gmail.com>
2016-10-04 00:22:21 +0300

    added new var + commented my code (kinda)

--- sct.js
+++ sct.js
@@ -1,20 +1,53 @@
+/*************************************
+ *              sct.js               *
+ *           By:  ItaySharon         *
+ *          License: GPL-3.0         *
+ *************************************/
+
+
+
+/*************************************
+ *              tag base             *
+ *************************************/
 const ots =               "<";
 const cts =               "</";
 const gte =               ">";
 
+/*************************************
+ *             tag names             *
+ *************************************/
 const ptn =               "html";
 const htn =               "head";
 const btn =               "body";
 
+/*************************************
+ *             open tags             *
+ *************************************/
 const sop =               ots + ptn + gte;
 const soh =               ots + htn + gte;
 const sob =               ots + btn + gte;
 
+/*************************************
+ *             close tags            *
+ *************************************/
 const eop =               cts + ptn + gte;
 const eoh =               cts + htn + gte;
 const eob =               cts + btn + gte;
 
+/*************************************
+ *          base page - TEST         *
+ *************************************/
+const bps =               sop + soh + eoh + sob;
+const bpe =               eob + eop;
+
+/*************************************
+ *          Unit Test Node           *
+ *************************************/
 const utn =               "OK";
+
+/*************************************
+ *              exports              *
+ *************************************/
 module.exports = {
 	ots: ots,
 	cts: cts,
@@ -31,6 +64,9 @@ module.exports = {
 	eop: eop,
 	eoh: eoh,
 	eob: eob,
+	
+	bps: bps,
+	bpe: bpe,
 	
 	utn: utn
 }
